086f679a4ce21679e9df1c8c9fecc37bdb5e894d
mar-celi <marcelideoliveirapaula@gmail.com>
2026-09-09 11:03:30 -0300

    ok

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded" , ()=>{
+    const likeBtn = document.querySelector(".like-btn");
+    const postMedia = document.querySelector(".post-media");
+    if (!likeBtn) return;
+
+    const likesCountSpan = likeBtn.querySelector(".likes-count");
+    const bookmarkBtn = document.querySelector(".bookmark-btn");
+
+    let isLiked = false;
+    let baseLikes = 0;
+    
+})
